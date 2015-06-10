d95eff646590229ba187cd031c2799c271a6fc93
watanabeko <ktarou569@gmail.com>
2015-06-10 12:29:17 +0000

    append variable,yield

--- basis/variable.rb
+++ basis/variable.rb
@@ -1,4 +1,3 @@
-
 # 変数
 var = 10
 var2 = "Hoge"
@@ -10,3 +9,14 @@ var3,var4 = "Huga","Piyo"
 
 puts var3
 puts var4
+
+# ローカルブロック変数
+# ブロック変数;のあとに記述
+[1,2,3,4,5].each do |num; var|
+  var = num
+  # ローカルブロック変数表示
+  puts "local_block:#{var}"
+end
+
+# ローカル変数表示
+puts "local:#{var}"
--- basis/yield.rb
+++ basis/yield.rb
@@ -19,3 +19,61 @@ def hoge
     yield
 end
 =end
+
+puts "---"
+
+def total(from, to)
+  result = 0
+  from.upto(to) do |num|
+    if block_given?
+      result += yield(num)
+    elsif
+      result += num
+    end
+  end
+  return result
+end
+
+# ブロックなしの呼び出し
+puts total(1,10)
+
+# ブロック有りの呼び出し(べき乗)
+n = total(1,10) do |num|
+  num ** 2
+end
+puts n
+
+puts total(1,10) { |num|
+  num ** 2
+}
+
+puts "---"
+
+# Procクラスによるブロックの保持
+hello = Proc.new do |var|
+  puts var
+end
+
+hello.call("Hello!")
+
+puts "---"
+
+# yieldの代わりに&を利用しブロックをコールする
+# &を指定するとブロックが自動的に保持される(内部ではProcクラスのインスタンスが作成される)．
+# なお，メソッドの一番最後に定義する．
+def total2(from, to, &block)
+
+  result = 0
+  from.upto(to) do |num|
+    if block
+      result += block.call(num)
+    elsif
+      result += num
+    end
+  end
+  return result
+end
+
+puts total2(1,10) { |num|
+  num ** 2
+}
