e2e527a5550317b465c0dcbe0eff6218382e028b
watanabeko <ktarou569@gmail.com>
2015-06-11 14:36:57 +0000

    FizzBuzz!

---
+++ junks/JunkFizzBuzz.rb
@@ -0,0 +1,43 @@
+class JunkFizzBuzz
+  @arr
+
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def if()
+    @arr.each do |num|
+      if num % 3 == 0 && num % 5 ==0
+        puts "FizzBuzz : #{num}"
+      elsif num % 3 == 0
+        puts "Fizz : #{num}"
+      elsif num % 5 == 0
+        puts "Buzz : #{num}"
+      else
+        puts num
+      end
+    end
+  end
+
+  def case
+    for num in @arr
+      case 0
+        when num % 15
+          puts "FizzBuzz : #{num}"
+        when num % 3
+          puts "Fizz : #{num}"
+        when num % 5
+          puts "Buzz : #{num}"
+        else
+          puts num
+      end
+    end
+  end
+
+  attr_accessor :arr
+end
+
+fizzbuzz = JunkFizzBuzz.new((0..100).to_a)
+
+fizzbuzz.if 
+fizzbuzz.case
