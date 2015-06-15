901044a02b6d6b4f8379e82efcb1ef920473471d
--global <ktarou569@gmail.com>
2015-06-15 14:58:36 +0000

    get params

--- web-apps/sinatra-sample/myapp.rb
+++ web-apps/sinatra-sample/myapp.rb
@@ -4,3 +4,14 @@ set:environment,:production
 get '/' do
   "Hello World"
 end
+
+# パラメータ受け取り
+get '/:param' do
+  # ハッシュ値paramsを利用
+  "#{params['param']}"
+end
+
+# ブロックにパラメータ値を渡すこともできる
+get '/block/:param' do |var|
+  "block: #{var}"
+end
