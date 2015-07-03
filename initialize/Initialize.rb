7f590ab5950f6242f352b4676c209b4c282d5be2
--global <ktarou569@gmail.com>
2015-07-03 17:31:34 +0000

    append method chain

--- initialize/Initialize.rb
+++ initialize/Initialize.rb
@@ -1,9 +1,10 @@
 
 class Parent
-  def initialize(name = "parent")
+  def initialize(name = "parent", age = 24)
     @name = name
+    @age = age
   end
-  attr_reader :name
+  attr_reader :name, :age
 
   def getName
     @name
@@ -34,9 +35,13 @@ p child2.class.ancestors
 
 # initializeメソッドもインスタンスメソッドなので下位のクラスからルックアップが始まる
 p child1.name
+# メソッド探索的にインスタンス変数ageは初期化されない => nil
+p child1.age
 
 # Parentクラスのinitializeメソッドが呼ばれインスタンス変数が初期化される
 p child2.name
+# メソッド探索としてインスタンス変数ageは初期化される => 24
+p child2.age
 
 # インスタンス変数は親クラスとも共存しているため，子クラスのメソッドでセッターを呼び出し
 # 親クラスで定義されているメソッドを呼び出しても同じ値が変える
